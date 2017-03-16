84774b3e1202ff206a0e47cb03a73f4cb9a4c3a1
Tanya Lyakh <Ttanka1110girl@gmail.com>
2017-03-16 13:26:03 +0100

    Finished

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 function Toggler(selector){
 	this._object = document.querySelector(selector);
+	this._object.style.display = "none";
 };
+
+Toggler.prototype.getElem = function(){
+	return this._object;
+}
+
+Toggler.prototype.show = function(){
+	
+	if (this._object.style.display === "none"){
+		
+		return this._object.style.display = "block";
+	}
+	return;
+}
+
+
+Toggler.prototype.hide = function(){
+	if (this._object.style.display = "block"){
+		return this._object.style.display = "none";
+	}
+	return;
+}
 
 var elem = new Toggler("#section");
 var button = document.querySelector("#button");
